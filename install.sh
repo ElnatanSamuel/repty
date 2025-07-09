bd61434e291667753f2bf5d31b4680bcff05eda8
Elnatan Samuel <elnatansamuel22@gmail.com>
2025-07-09 12:41:22 +0300

    export path

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ rm -rf "$INSTALL_DIR"
 echo "Cloning repo $REPO_URL into $INSTALL_DIR"
 pwd
 
-
 git clone --branch dev-el "$REPO_URL" "$INSTALL_DIR" || {
   echo "Failed to clone repo"; exit 1;
 }
@@ -32,6 +31,7 @@ if [[ $SHELL == */zsh ]]; then
     echo 'repty_log() { "$HOME/.repty/bin/repty" log "$?" ; }'
     echo 'precmd() { repty_log }'
   } >> "$HOME/.zshrc"
+  SHELL_RC="$HOME/.zshrc"
 elif [[ $SHELL == */bash ]]; then
   echo 'Adding Repty to .bashrc...'
   {
@@ -39,8 +39,20 @@ elif [[ $SHELL == */bash ]]; then
     echo 'repty_log() { "$HOME/.repty/bin/repty" log "$?" ; }'
     echo 'PROMPT_COMMAND="repty_log"'
   } >> "$HOME/.bashrc"
+  SHELL_RC="$HOME/.bashrc"
 else
   echo "Unknown shell. Manual setup might be needed."
+  SHELL_RC=""
+fi
+
+# Add repty bin directory to PATH if not already added
+if [[ -n "$SHELL_RC" ]]; then
+  if ! grep -q 'export PATH="$HOME/.repty/bin:$PATH"' "$SHELL_RC"; then
+    echo 'export PATH="$HOME/.repty/bin:$PATH"' >> "$SHELL_RC"
+    echo "Added repty bin directory to PATH in $SHELL_RC"
+  else
+    echo "PATH to repty bin already present in $SHELL_RC"
+  fi
 fi
 
 echo "Repty has been installed. Restart your terminal or run 'source ~/.zshrc' or 'source ~/.bashrc'"
