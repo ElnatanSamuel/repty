6907722629ed69bc0118b7545b8b292a1a5e74c2
Elnatan Samuel <elnatansamuel22@gmail.com>
2025-07-09 13:15:41 +0300

    uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -7,13 +7,17 @@ rm -f "$HOME/repty_history.md"
 rm -f "$HOME/.repty.db"
 
 if [ -f "$HOME/.zshrc" ]; then
+  echo "Cleaning up .zshrc..."
   sed -i '/repty_log()/,/^}/d' "$HOME/.zshrc"
   sed -i '/precmd() { repty_log }/d' "$HOME/.zshrc"
+  sed -i '/export PATH="$HOME\/.repty\/bin:$PATH"/d' "$HOME/.zshrc"
 fi
 
 if [ -f "$HOME/.bashrc" ]; then
+  echo "Cleaning up .bashrc..."
   sed -i '/repty_log()/,/^}/d' "$HOME/.bashrc"
   sed -i '/PROMPT_COMMAND="repty_log"/d' "$HOME/.bashrc"
+  sed -i '/export PATH="$HOME\/.repty\/bin:$PATH"/d' "$HOME/.bashrc"
 fi
 
 echo "Repty has been removed. Restart your terminal to finish cleanup."
